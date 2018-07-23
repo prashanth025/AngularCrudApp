ad268ce8f36f4dbcd5ee51565cef765a73d6d16d
prashantha.r <prashantha.r@robosoftin.com>
2018-07-23 20:59:00 +0530

    read employee data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListemployeesComponent } from './employees/listemployees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListemployeesComponent
   ],
   imports: [
     BrowserModule
