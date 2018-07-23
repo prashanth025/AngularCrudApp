ffa475d91956a1c67c7bbd99756f0ee5454074ce
prashanth <rprashanth025@gmail.com>
2018-07-24 00:29:52 +0530

    createEmployee component is added

---
+++ src/app/employees/create.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  saveEmployee(empForm:NgForm):void{
+    console.log(empForm.value);
+  }
+}
